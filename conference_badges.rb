bbd078ddc25b279622a2ddfdaa1d198071d54073
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 21:50:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,6 +29,14 @@ def assign_rooms(speakers)
   end 
 
 
+def printer(batch_badge_creator, assign_rooms)
+    batch_badge_creator.each do |y|
+      puts y 
+    end 
+    assign_rooms do |z|
+      puts z
+    end
+end 
 
 
 
