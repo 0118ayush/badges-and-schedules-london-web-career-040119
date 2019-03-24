aac79dc0dd042eeb1c1f1b4cea852d0d7f6e1b00
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 21:22:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,9 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 end 
-
-
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def batch_badge_creator(speakers)
@@ -15,3 +13,6 @@ def batch_badge_creator(speakers)
   end 
   badgemessages
 end 
+
+
+def 
